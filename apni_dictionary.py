c1489e0d40c9c9ae8e808a807e55e89ef538af85
Bheda Sakshi <84337482+SakshiBheda@users.noreply.github.com>
2021-06-22 02:29:12 -0700

    Add files via upload

---
+++ apni_dictionary.py
@@ -0,0 +1,10 @@
+print("This is My own Dictionary : 'Apni Dictionary'")
+print("key ,"" book "", comb "" ,mouse "" , joey ")
+dic1={"key":"something which unlocks a lock",
+      "book":"a compiled set of pages which describes a certain topic",
+      "comb":"a plastic item to set hairs",
+      "mouse":"an animal which is afraid of cats",
+      "joey":"kid of a kangroo"}
+print("Please enter the word whose meaning you want")
+ax=input()
+print("It means : " ,dic1[ax])
